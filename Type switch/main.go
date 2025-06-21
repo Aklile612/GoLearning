d746ac2d6f96a92c4adac8998dd9181db35308f7
Aklile Ansa <aklilemente72@gmail.com>
2025-06-21 23:08:52 +0300

    first commit

--- Type switch/main.go
+++ Type switch/main.go
@@ -1,2 +1,19 @@
 package main
 
+import "fmt"
+
+func main() {
+
+	var a interface{} = "aklile is manutd"
+
+	switch value := a.(type) {
+	case int64:
+		fmt.Println("This is an int",value)
+	case float64:
+		fmt.Println("This is a floating number",value)
+	case string:
+		fmt.Println("This is a string ",value)
+	default:
+		fmt.Println("Type Unkown",value)
+	}
+}
