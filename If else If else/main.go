2a54f37d9ea1eaea7a5f3fd2cdea5b822afb815a
Aklile Ansa <aklilemente72@gmail.com>
2025-06-21 22:51:09 +0300

    first commit

--- If else If else/main.go
+++ If else If else/main.go
@@ -1,1 +1,18 @@
 package main
+
+import "fmt"
+
+func main() {
+
+	a := 10
+
+	b := 10
+
+	if (a < b) {
+		fmt.Println("A less than B")
+	}else if (a==b){
+		fmt.Println("A equal to B")
+	}else{
+		fmt.Println("A greater than B")
+	}
+}
