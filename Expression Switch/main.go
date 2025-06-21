11829133f54a80ef1bfd67e9685d3979564cd0f5
Aklile Ansa <aklilemente72@gmail.com>
2025-06-21 23:00:56 +0300

    first commit

--- Expression Switch/main.go
+++ Expression Switch/main.go
@@ -1,6 +1,16 @@
 package main
 
+import "fmt"
 
-func main(){
-	
+func main() {
+	a := 20
+
+	switch {
+	case a == 1:
+		fmt.Println("The first day")
+	case a == 2:
+		fmt.Println("the second day")
+	default:
+		fmt.Println("This is the End hold your breath that we make ")
+	}
 }
