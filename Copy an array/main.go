613f9cc2f9f8f787d0daf439d5b68b32837c7822
Aklile Ansa <aklilemente72@gmail.com>
2025-06-23 11:57:34 +0300

    first commit

--- Copy an array/main.go
+++ Copy an array/main.go
@@ -1,2 +1,19 @@
 package main
 
+import "fmt"
+
+func main() {
+
+	array1 := [3]string{"a", "b", "c"}
+
+	array2 := array1
+
+	array3 := &array1
+
+	fmt.Printf("Array1: %v\n", array1)
+	fmt.Printf("Array1: %v\n", array2)
+
+	array1[0]="z"
+	fmt.Printf("Array1: %v\n", array2)
+	fmt.Printf("Array1: %v\n", *array3)
+}
