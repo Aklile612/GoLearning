c0b86b39f7a9cd781be6c416e6003f2a43839c97
Aklile Ansa <aklilemente72@gmail.com>
2025-06-23 19:24:16 +0300

    first commit

--- slice Literal/main.go
+++ slice Literal/main.go
@@ -1,2 +1,15 @@
 package main
 
+import "fmt"
+
+func main() {
+
+	var slice1 = []string{"A", "B", "C"}
+
+	fmt.Println("slice1", slice1)
+
+	slice2 := []int{10, 20, 40}
+
+	fmt.Println("slice1", slice2)
+
+}
