d63955bb6da1cc55b93881c98e8e099a3bb63f5f
Aklile Ansa <aklilemente72@gmail.com>
2025-06-23 11:20:46 +0300

    first commit

--- creating Array/main.go
+++ creating Array/main.go
@@ -1,3 +1,18 @@
 package main
 
+import "fmt"
 
+func main() {
+
+	var students [3]string
+
+	students[0] = "aklile"
+
+	students[1] = "abebe"
+
+	students[2] = "bekele"
+
+	for i := 0; i < len(students); i++ {
+		fmt.Println("names: ", students[i])
+	}
+}
