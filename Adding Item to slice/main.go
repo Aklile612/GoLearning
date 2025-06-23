a3cd8022416ad25bdee679ce17f61d047fd59d6b
Aklile Ansa <aklilemente72@gmail.com>
2025-06-23 19:57:11 +0300

    first commit

--- Adding Item to slice/main.go
+++ Adding Item to slice/main.go
@@ -1,2 +1,18 @@
 package main
 
+import "fmt"
+
+func main() {
+
+	a := make([]int, 2, 5)
+
+	a[0] = 2
+	a[1] = 5
+
+	fmt.Println("Slice before", a)
+
+	a=append(a, 3,4,5,6,7,8)
+
+	fmt.Println("Slice after", a)
+
+}
