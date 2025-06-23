8d0859cd281ddcf5e904f2b2308937ec5e9a8ac2
Aklile Ansa <aklilemente72@gmail.com>
2025-06-23 19:44:07 +0300

    first commit

--- slice using make/main.go
+++ slice using make/main.go
@@ -1,1 +1,14 @@
 package main
+
+import "fmt"
+
+func main() {
+
+	var slice1 = make([]int, 4, 7)
+
+	fmt.Printf("slice1= %v\t, Lenght= %d\t , capacity=%d\n", slice1, len(slice1), cap(slice1))
+
+	slice2:= make([]int,4)
+	fmt.Printf("slice2= %v\t, Lenght= %d\t , capacity=%d\n", slice2,len(slice2),cap(slice2))
+
+}
