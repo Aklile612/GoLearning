ba753b0b85417fc1c75595b137de6fdb0046ccd5
Aklile Ansa <aklilemente72@gmail.com>
2025-06-23 19:33:38 +0300

    first commit

--- slice from slice/main.go
+++ slice from slice/main.go
@@ -1,6 +1,12 @@
 package main
 
-func main(){
+import "fmt"
 
-	var mainslice
+func main() {
+
+	var mainslice = []int{1, 23, 4, 5, 67, 8, 5}
+
+	newslice := mainslice[3:6]
+
+	fmt.Println("New slice: ", newslice)
 }
