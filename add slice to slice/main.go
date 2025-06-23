a22ef6685c09d41bbe5b9fa3a48b9ceb33e41915
Aklile Ansa <aklilemente72@gmail.com>
2025-06-23 20:16:26 +0300

    first commit

--- add slice to slice/main.go
+++ add slice to slice/main.go
@@ -1,1 +1,14 @@
 package main
+
+import "fmt"
+
+func main() {
+
+	slice1 := []int{10, 20, 30}
+
+	slice2 := []int{40, 50, 60}
+
+	slice2 = append(slice2, slice1...)
+
+	fmt.Println("slice 2", slice2)
+}
