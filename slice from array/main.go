324f7cf39923bd53254d6ccfbdd833f9fa3c9085
Aklile Ansa <aklilemente72@gmail.com>
2025-06-23 19:30:03 +0300

    first commit

--- slice from array/main.go
+++ slice from array/main.go
@@ -1,8 +1,12 @@
 package main
 
-func main(){
+import "fmt"
 
-	array:= [5]int {1,3,4,5,6}
+func main() {
 
-	
+	array := [5]int{1, 3, 4, 5, 6}
+
+	newslice := array[3:5]
+
+	fmt.Println("new slice", newslice)
 }
