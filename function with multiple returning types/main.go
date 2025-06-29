04f2f9b067224a65a784ea87919c23442c0762e8
Aklile Ansa <aklilemente72@gmail.com>
2025-06-29 22:00:27 +0300

    first commit

--- function with multiple returning types/main.go
+++ function with multiple returning types/main.go
@@ -1,3 +1,21 @@
 package main
 
+import "fmt"
 
+func rectangle(w int, h int) (int, int) {
+
+	perimeter := 2 * (w + h)
+
+	area := w * h
+
+	return area, perimeter
+}
+
+func main() {
+
+	a, p := rectangle(3, 6)
+
+	fmt.Println("Area:", a)
+
+	fmt.Println("Permeter: ", p)
+}
