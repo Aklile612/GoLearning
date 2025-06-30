b5a127d892b66dd913c2622849cf237619817aa5
Aklile Ansa <aklilemente72@gmail.com>
2025-06-30 19:29:06 +0300

    first commit

--- initalizing map using make/main.go
+++ initalizing map using make/main.go
@@ -1,2 +1,15 @@
 package main
 
+import "fmt"
+
+func main() {
+
+	var myMap = make(map[string]float64)
+
+	myMap["a"] = 98
+	myMap["N"] = 86.75
+	myMap["Na"] = 80.5
+	myMap["ya"] = 79.8
+
+	fmt.Println("The total thing is: ", myMap)
+}
