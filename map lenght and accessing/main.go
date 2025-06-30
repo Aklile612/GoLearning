16cdd996e32c25142a7ab11ced1475b1af071dd7
Aklile Ansa <aklilemente72@gmail.com>
2025-06-30 19:37:01 +0300

    first commit

--- map lenght and accessing/main.go
+++ map lenght and accessing/main.go
@@ -1,3 +1,20 @@
 package main
 
+import "fmt"
 
+func main() {
+
+	empolyee := make(map[string]int)
+
+	empolyee["A"] = 1
+	empolyee["B"] = 10
+	empolyee["C"] = 19
+	empolyee["D"] = 20
+
+	empolyeeList := make(map[string]int)
+
+	fmt.Println("Employee: ", empolyee)
+
+	fmt.Println("Employee List", empolyeeList)
+
+}
