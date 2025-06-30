7bc462b217e6e8d5c27bd6ae8d3af91a985bde95
Aklile Ansa <aklilemente72@gmail.com>
2025-06-30 22:45:37 +0300

    first commit

--- sort a map/main.go
+++ sort a map/main.go
@@ -1,22 +1,29 @@
 package main
 
-import "sort"
+import (
+	"fmt"
+	"sort"
+)
 
-func main(){
+func main() {
 
-	unsortedArray:= make(map[int]string)
+	unsortedArray := make(map[int]string)
 
-	unsortedArray[50]="A"
-	unsortedArray[30]="C"
-	unsortedArray[40]="B"
+	unsortedArray[50] = "A"
+	unsortedArray[30] = "C"
+	unsortedArray[40] = "B"
 
-	unsortedArray[20]="D"
+	unsortedArray[20] = "D"
 
-	arr:=make([]int ,0,len(unsortedArray))
+	arr := make([]int, 0, len(unsortedArray))
 
-	for i := range(unsortedArray){
+	for i := range unsortedArray {
 		arr = append(arr, i)
 	}
 
 	sort.Ints(arr)
+
+	for _, values := range arr {
+		fmt.Println(unsortedArray[values], values)
+	}
 }
