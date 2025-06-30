63699799aeb6e9c39af68c62935d00526d09f76b
Aklile Ansa <aklilemente72@gmail.com>
2025-06-30 19:21:30 +0300

    first commit

--- initalizing map/main.go
+++ initalizing map/main.go
@@ -7,4 +7,13 @@ func main() {
 	var emptyMap = map[int]string{}
 
 	fmt.Println("Empty map: ", emptyMap)
+
+	var myMap = map[int]string{
+		1:"A",
+		2:"B",
+		3:"C",
+		4:"D",
+	}
+
+	fmt.Println("My map: ",myMap)
 }
