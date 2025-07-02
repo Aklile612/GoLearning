d567400647e58dc739df86418972adae0c46785d
Aklile Ansa <aklilemente72@gmail.com>
2025-07-02 10:19:05 +0300

    first commit

--- assigning defult value for a struct/main.go
+++ assigning defult value for a struct/main.go
@@ -1,1 +1,31 @@
 package main
+
+import "fmt"
+
+type student struct {
+	name string
+	age  int
+}
+
+func (s *student) Instance() {
+	if s.name == "" {
+		s.name = "Robert"
+	}
+
+	if s.age == 0 {
+		s.age = 25
+	}
+}
+
+func main() {
+	student1 := student{}
+
+	student1.Instance()
+	fmt.Println(student1)
+
+	student2:= student{
+		name: "aklile",
+	}
+	student2.Instance()
+	fmt.Println(student2)
+}
