25ee67e9a2a2c2288b7aafc5b3d712c8f499d16a
Aklile Ansa <aklilemente72@gmail.com>
2025-07-02 11:07:24 +0300

    first commit

--- type assertion/main.go
+++ type assertion/main.go
@@ -1,1 +1,16 @@
 package main
+
+import "fmt"
+
+func main() {
+	var result1 =" Go shut ur damn mouth"
+	display(result1)
+	
+	result2:=1223
+	display(result2)
+}
+func display(a interface{}) {
+	value,ok := a.(string)
+
+	fmt.Printf("Value:%v --- status is %v\n ", value,ok)
+}
