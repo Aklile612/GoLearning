b18bcab41c578fe3ed4352a9d4c9def5cf0bce55
Aklile Ansa <aklilemente72@gmail.com>
2025-07-02 10:13:53 +0300

    first commit

--- add method to struct/main.go
+++ add method to struct/main.go
@@ -1,1 +1,23 @@
 package main
+
+import "fmt"
+
+type work struct {
+	branch string
+	name   string
+	salary int
+}
+
+func (a work) show() {
+
+	fmt.Println("Branch: ", a.branch)
+}
+func main() {
+	result := work{
+		branch: "lafto",
+		name:   "aklile",
+		salary: 2000,
+	}
+
+	result.show()
+}
