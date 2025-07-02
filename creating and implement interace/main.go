c4436b5b109d3f98d930e5b7a3166483f458feb2
Aklile Ansa <aklilemente72@gmail.com>
2025-07-02 10:49:57 +0300

    first commit

--- creating and implement interace/main.go
+++ creating and implement interace/main.go
@@ -1,23 +1,28 @@
 package main
-//Rectange Interface
-type Rectangle interface{
+
+import "fmt"
+
+// Rectange Interface
+type Rectangle interface {
 	Permeter() float64
 	Area() float64
 }
 
-type Calculate struct{
+type Calculate struct {
 	length float64
-	width float64
+	width  float64
 }
 
-func (c Calculate) Permeter()float64{
-	return (c.length+c.width)*2
+func (c Calculate) Permeter() float64 {
+	return (c.length + c.width) * 2
 }
 
-func (c Calculate) Area() float64{
-	return (c.length*c.width)
+func (c Calculate) Area() float64 {
+	return (c.length * c.width)
 }
 func main() {
 	var r Rectangle
-	
+	r = Calculate{10, 20}
+	fmt.Println("Area", r.Area())
+	fmt.Println("Permeter", r.Permeter())
 }
