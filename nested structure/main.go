44e03bdca42650ec1c6e2ab093199bc1405ae8d2
Aklile Ansa <aklilemente72@gmail.com>
2025-07-02 10:00:13 +0300

    first commit

--- nested structure/main.go
+++ nested structure/main.go
@@ -1,15 +1,23 @@
 package main
 
-//Author struct
-type Authour struct{
-	name string
+import "fmt"
+
+// Author struct
+type Authour struct {
+	name   string
 	branch string
-	year int
+	year   int
 }
 
-type HR struct{
+type HR struct {
 	details Authour
 }
+
 func main() {
-	
+
+	result := HR{
+		details: Authour{"aklile", "lafto", 2004},
+	}
+
+	fmt.Println("Result:", result)
 }
