f62f21d11357b649175c95e25a6234f4f980d724
Aklile Ansa <aklilemente72@gmail.com>
2025-07-02 10:42:28 +0300

    first commit

--- creating and implement interace/main.go
+++ creating and implement interace/main.go
@@ -1,1 +1,15 @@
 package main
+//Rectange Interface
+type Rectangle interface{
+	Permeter() float64
+	Area() float64
+}
+
+type Calculate struct{
+	length float64
+	width float64
+}
+
+func main() {
+	
+}
