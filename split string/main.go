49e23d123d4f29e041bcaa2d3d18ac3c0606de24
Aklile Ansa <aklilemente72@gmail.com>
2025-07-07 11:01:40 +0300

    edited

--- split string/main.go
+++ split string/main.go
@@ -1,6 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
 
-func main(){
-	
+func main() {
+	str1 := "This,is,the,fact,that,we,are,tired"
+
+	str2 := "THis should be the only way"
+
+	result1 := strings.Split(str1, ",")
+	result2 := strings.Split(str2, "")
+	fmt.Println(result1)
+	fmt.Println(result2)
 }
