f5ab493ad9fea70613b4893e7e035fdc11c5f92a
Aklile Ansa <aklilemente72@gmail.com>
2025-07-07 11:23:37 +0300

    edited

--- contains string/main.go
+++ contains string/main.go
@@ -12,4 +12,10 @@ func main() {
 	fmt.Println(strings.Contains("Germany","GER"))
 
 	//Fields
+
+	message:="Abebe Beso Libela Blo"
+
+	messageArray:=strings.Fields(message)
+
+	fmt.Println(messageArray)
 }
