aa0448c1f8527e2390f19debd33248d7d9236157
Aklile Ansa <aklilemente72@gmail.com>
2025-07-07 11:49:40 +0300

    edited

--- scan fmt/main.go
+++ scan fmt/main.go
@@ -1,1 +1,16 @@
 package main
+
+import "fmt"
+func main() {
+	var name string
+	var age int
+	var height float64
+	var isAbsent bool
+
+	fmt.Scan(&name)
+	fmt.Scan(&height)
+	fmt.Scan(&age)
+	fmt.Scan(&isAbsent)
+
+	fmt.Printf("%s %d %f %t", name,age,height,isAbsent)
+}
