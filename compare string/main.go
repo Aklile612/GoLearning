eab1f4eb0bf3a40e60e604b2c7bfd2e3e42de177
Aklile Ansa <aklilemente72@gmail.com>
2025-07-07 11:15:30 +0300

    edited

--- compare string/main.go
+++ compare string/main.go
@@ -9,4 +9,11 @@ func main() {
 	result1 := strings.Compare("Hello", "Hello")
 
 	fmt.Println(result1)
+
+	result2:= strings.Compare("Hello","Aklile")
+	fmt.Println(result2)
+	result3 := strings.Compare("Hello", "hello")
+
+	fmt.Println(result3)
+
 }
