6a35d4d4c38cd2377239654b4462133bb5e91d54
Aklile Ansa <aklilemente72@gmail.com>
2025-07-07 10:52:24 +0300

    edited

--- Trim string/main.go
+++ Trim string/main.go
@@ -13,4 +13,11 @@ func main() {
 	fmt.Println(trim1)
 
 	//trim space
+
+	result2:= "   welcome to the hustle   "
+
+	trim2:= strings.TrimSpace(result2)
+
+	fmt.Println("Trimed after space",trim2)
+
 }
