159a85b0b3bed7cc763cac1580526feaf90a5182
Aklile Ansa <aklilemente72@gmail.com>
2025-07-09 17:32:34 +0300

    edited

--- create file/main.go
+++ create file/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
 func main() {
-	
+	emptyFile,err:= os.Create("empty.txt")
+
+	if err!= nil{
+		log.Fatal(err)
+	}else{
+		log.Println(emptyFile)
+	}
 }
